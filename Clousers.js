514c374b2bb95de165cb6a5590987a9c34bce6c6
Himanshujena2003 <himanshujena2003@gmail.com>
2026-05-25 14:02:45 +0530

    new change

--- Clousers.js
+++ Clousers.js
@@ -1,12 +1,26 @@
 // Clouser is a function bundeled up with it's lexical scope (function + lexical scope)
 
-function x(){
-    var a = 10
-    function y(){
-        console.log(a)
-    }
-    return y;
-}
+// function x(){
+//     var a = 10
+//     function y(){
+//         console.log(a)
+//     }
+//     return y;
+// }
 
-const z = x();
-z();
+// const z = x();
+// z();
+
+function x(){
+    for(var i=1;i<=6;i++){
+        function y(x){   // due to the clouser the setTimeout remembers its lexical scope
+            setTimeout(()=>{
+                console.log(x);
+            },i*1000)
+        }
+        y(i)
+    }
+
+    console.log("Hello student")
+}
+x()
